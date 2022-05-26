4e3b122fdef65079635a16e268bed2eed3b5504c
Teresa Goade <teresa.goade@gmail.com>
2022-05-26 16:33:17 -0400

    change settings for pushing to production

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyAwEshkuNVasDUYZX8Es3sJE5_O6CP5Q4o",
+    authDomain: "plant-inventory-21562.firebaseapp.com",
+    databaseURL: "https://auth-development-ce842-default-rtdb.firebaseio.com",
+    projectId: "plant-inventory-21562",
+    storageBucket: "plant-inventory-21562.appspot.com",
+    messagingSenderId: "27312914912",
+    appId: "1:27312914912:web:7e7df1a7894e03563aab85"
   };
 
   // Initialize Firebase and Firestore
